06e095c0ed74c659d21702ac7bdc7363fa5225a3
b.smirnov <besm1@yandex.ru>
2024-10-29 22:04:17 +0300

    Logging

--- tests_12_1.py
+++ tests_12_1.py
@@ -4,6 +4,7 @@ import unittest
 class RunnerTest(unittest.TestCase):
     is_frozen = False
 
+    @skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         rn = runner.Runner('Ипполит Матвеевич Воробьянинов')
         for i in range(10):
@@ -11,12 +12,14 @@ class RunnerTest(unittest.TestCase):
         print(self.assertEqual(rn.distance, 50))
         self.assertEqual(rn.distance, 50)
 
+    @skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         rn = runner.Runner('Конрад Карлович Михельсон')
         for i in range(10):
             rn.run()
         self.assertEqual(rn.distance, 100)
 
+    @skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         bender = runner.Runner('Остап Сулейман Берта Мария Бендер Бей')
         koreiko = runner.Runner("Александр Иванович Корейко")
