e362872a8d42cbe811914c7af689c06aa9b470ad
b.smirnov <besm1@yandex.ru>
2024-11-01 22:01:45 +0300

    Logging tests_12_4a

---
+++ test_12_4a.py
@@ -0,0 +1,53 @@
+# import runner
+import logging
+
+import rt_with_exceptions as runner
+import unittest
+
+logging.basicConfig(level=logging.DEBUG, filemode='w', filename='runner_test.log', encoding='utf8',
+                    format="%(asctime)s %(levelname)s %(funcName)s. %(lineno)d: %(message)s")
+logging.info('Start!')
+# n = RunnerTest()
+# unittest.main()
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            rn = runner.Runner('Ипполит Матвеевич Воробьянинов', speed=-10)
+            for i in range(10):
+                rn.walk()
+            print(self.assertEqual(rn.distance, 50))
+            self.assertEqual(rn.distance, 50)
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError as e:
+            logging.info(f'Неверная скорость для Runner: {e}')
+
+
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        try:
+            rn = runner.Runner(name=3.62, speed='Конрад Карлович Михельсон')
+            for i in range(10):
+                rn.run()
+            self.assertEqual(rn.distance, 100)
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as e:
+            logging.info(f'Неверный тип данных для объекта Runner: {e}')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_tour(self):
+        bender = runner.Runner('Остап Сулейман Берта Мария Бендер Бей')
+        koreiko = runner.Runner("Александр Иванович Корейко")
+        for i in range(10):
+            bender.walk()
+            koreiko.run()
+        self.assertNotEqual(bender.distance, koreiko.distance)
+
+#
+# if __name__ == '__main__':
+#     unittest.main()
--- tests_12_4.py
+++ tests_12_4.py
@@ -5,11 +5,6 @@ from tests_12_1 import RunnerTest
 
 
 if __name__ == '__main__':
-    # Уровень - INFO
-    # Режим - запись с заменой('w')
-    # Название файла - runner_tests.log
-    # Кодировка - UTF-8
-    # Формат вывода - на своё усмотрение, обязательная информация: уровень логирования, сообщение логирования.
     logging.basicConfig(level=logging.DEBUG, filemode='w', filename='runner_test.log', encoding='utf8',
                          format="%(asctime)s %(levelname)s %(funcName)s. %(lineno)d: %(message)s")
     logging.info('Start!')
